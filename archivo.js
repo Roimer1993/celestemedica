c8f2a5d34995777b640e066bb2e0663d71ad3220
Roimer1993 <139517506+Roimer1993@users.noreply.github.com>
2024-02-21 16:15:16 -0300

    se agrega comentario de la funtion del boton

--- archivo.js
+++ archivo.js
@@ -2,4 +2,9 @@ document.getElementById('search-icon').addEventListener('click', function() {
     var searchBox = document.getElementById('search-box');
     searchBox.style.display = (searchBox.style.display === 'block') ? 'none' : 'block';
   });
-  
+/* document.getElementById('search-icon'): Esto selecciona el elemento del DOM con el id 'search-icon'. En tu caso, es el enlace (<a>) que contiene la imagen de búsqueda.
+.addEventListener('click', function() { ... });: Agrega un "escuchador de eventos" al elemento seleccionado. En este caso, estamos escuchando el evento de clic (click). Cuando se hace clic en el elemento con id 'search-icon', se ejecutará la función que viene a continuación.
+var searchBox = document.getElementById('search-box');: Aquí se selecciona el elemento del DOM con el id 'search-box'. Este elemento corresponde al contenedor de la caja de búsqueda.
+searchBox.style.display = (searchBox.style.display === 'block') ? 'none' : 'block';: Este código verifica el estilo actual de la propiedad display del elemento 'search-box'. Si su valor es 'block', se establece a 'none' (oculto); si es cualquier otra cosa que no sea 'block' (como 'none' o no está definido), se establece a 'block' (visible).
+En resumen, este bloque de código JavaScript agrega un escuchador de eventos al icono de búsqueda. Cuando se hace clic en el icono, la caja de búsqueda (search-box) se mostrará si está oculta o se ocultará si ya está visible. Esto proporciona un efecto de alternancia para mostrar u ocultar la caja de búsqueda al hacer clic en el icono.
+*/  
