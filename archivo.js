e1267caab142534599e0d4f81f115762ae6a5ec3
Roimer1993 <139517506+Roimer1993@users.noreply.github.com>
2024-02-21 16:09:12 -0300

    se hace el icono de busqueda desplegable

---
+++ archivo.js
@@ -0,0 +1,5 @@
+document.getElementById('search-icon').addEventListener('click', function() {
+    var searchBox = document.getElementById('search-box');
+    searchBox.style.display = (searchBox.style.display === 'block') ? 'none' : 'block';
+  });
+  
